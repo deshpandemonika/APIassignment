b4c9e9677283f6155c07f0a5068a0f96bd2c3168
Harshal Deshpande <Harshal Deshpande@HARSHAL>
2024-06-22 11:24:05 +0200

    API Commit

---
+++ MayAPIBatch/src/test/java/MayBatch/MayAPIBatch/GetAllTestcase.java
@@ -0,0 +1,11 @@
+package MayBatch.MayAPIBatch;
+
+import io.restassured.RestAssured;
+public class GetAllTestcase {
+	public static void main(String[] args) {
+      RestAssured a = new RestAssured();
+     
+		
+	}
+
+}
